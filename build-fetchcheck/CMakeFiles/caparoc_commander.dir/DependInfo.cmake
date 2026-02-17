f64be5d26d87201d93f9fbba1fd6105ea7ded8ab
Markus Werle <daixtrose@users.noreply.github.com>
2026-02-18 00:56:41 +0100

    Add g++-14 release build workflow

---
+++ build-fetchcheck/CMakeFiles/caparoc_commander.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/markus/PROJECTS/caparoc-utils/caparoc_commander/src/caparoc_commander.cpp" "CMakeFiles/caparoc_commander.dir/src/caparoc_commander.cpp.o" "gcc" "CMakeFiles/caparoc_commander.dir/src/caparoc_commander.cpp.o.d"
+  "/home/markus/PROJECTS/caparoc-utils/caparoc_commander/src/cli_parser.cpp" "CMakeFiles/caparoc_commander.dir/src/cli_parser.cpp.o" "gcc" "CMakeFiles/caparoc_commander.dir/src/cli_parser.cpp.o.d"
+  "/home/markus/PROJECTS/caparoc-utils/caparoc_commander/src/create_modbus_connection.cpp" "CMakeFiles/caparoc_commander.dir/src/create_modbus_connection.cpp.o" "gcc" "CMakeFiles/caparoc_commander.dir/src/create_modbus_connection.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-fetchcheck/_deps/libcaparoc_proj-subbuild/CMakeFiles/libcaparoc_proj-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for libcaparoc_proj-populate.
+# This may be replaced when dependencies are built.
